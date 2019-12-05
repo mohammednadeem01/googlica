f643c926d427666d16ab81c17064d7c34484836f
mohammednadeem01 <mohdnadeem01786@gmail.com>
2019-12-05 12:51:33 +0530

    removing date.js

--- date.js
+++
@@ -1,8 +0,0 @@
-const.printDate = () => {
-    const d = new Date()
-    const dd = d.getDate() + 1;
-    const mm = d.getMonth();
-    const yyyy = d.getFullYear();
-    return $[dd]/$[mm]/$[yyyy]
-
-}
